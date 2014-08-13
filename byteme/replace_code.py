350009dda5d04e9d47dd4a1d953f1f1556a89ec3
Allison Kaptur <allison.kaptur@gmail.com>
2014-08-13 16:07:42 -0400

    fixed jump targets

--- byteme/replace_code.py
+++ byteme/replace_code.py
@@ -1,43 +1,53 @@
 from types import CodeType
 import opcode
+import dis
 from opcode import opname, opmap
 
-def fact(n, accum):
-    if n <= 1:
-        return accum
-    else:
-        return fact(n-1, accum*n)
-
-def make_new_code(fn):
+def make_tail_recursive(fn):
     old_code = fn.__code__
     bytecode = tail_recurse(fn)
     c = CodeType(old_code.co_argcount, old_code.co_nlocals, old_code.co_stacksize,
             old_code.co_flags, bytecode, old_code.co_consts, old_code.co_names,
             old_code.co_varnames, old_code.co_filename, old_code.co_name, old_code.co_firstlineno,
             old_code.co_lnotab)
-    return c
+    fn.__code__ = c
+    return fn
 
 def tail_recurse(fn):
     new_bytecode = []
+    jump_displacement = 0
+    jump_list = []
     code_obj = fn.__code__
     for byte, arg in consume(code_obj.co_code):
         name = opcode.opname[byte]
-        print name, arg
-        # pdb.set_trace()
         if name == "LOAD_GLOBAL" and code_obj.co_names[arg] == fn.__name__:
-            pass # come back and fix jump targets
+            jump_displacement -= 3
         elif name == "CALL_FUNCTION":
             for i in range(arg): # 0, 1
                 new_bytecode.append(opmap["STORE_FAST"])
                 new_bytecode += split(arg - i - 1)
             new_bytecode.append(opmap["JUMP_ABSOLUTE"])
             new_bytecode += split(0) # jump to beginning of bytecode
+            jump_displacement += 3 * arg
         else:
             new_bytecode.append(byte)
             if arg is not None:
                 new_bytecode += split(arg)
+        if byte > opcode.HAVE_ARGUMENT:
+            jump_list.append(jump_displacement)
+            jump_list.append(jump_displacement)
+        jump_list.append(jump_displacement)
 
-    return "".join([chr(b) for b in new_bytecode])
+    newer_bytecode = []
+    for byte, arg in consume(new_bytecode):
+        if byte in opcode.hasjabs:
+            arg = arg + jump_list[arg]
+
+        newer_bytecode.append(byte)
+        if arg is not None:
+            newer_bytecode += split(arg)
+
+    return "".join([chr(b) for b in newer_bytecode])
 
 def split(num):
     """ Return an integer as two bytes"""
@@ -45,7 +55,8 @@ def split(num):
 
 
 def consume(bytecode):
-    bytecode = [ord(b) for b in bytecode]
+    if isinstance(bytecode[0], str):
+        bytecode = [ord(b) for b in bytecode]
     i = 0
     while i < len(bytecode):
         op = bytecode[i]
@@ -58,6 +69,34 @@ def consume(bytecode):
             yield op, None
             i += 1
 
+@make_tail_recursive
+def fact(n, accum):
+    if n <= 1:
+        return accum
+    else:
+        return fact(n-1, accum*n)
+
+def fact2(n, accum):
+    if n > 1:
+        return fact2(n-1, accum*n)
+    else:
+        return accum
+
+def fact3(n, accum):
+    x = fact3
+    if n <= 1:
+        return accum
+    else:
+        return fact3(n-1, accum*n)
+
+
+
+
 if __name__ == '__main__':
-    fact.__code__ = make_new_code(fact)
-    print fact(1000,1)
+    # print fact(1000,1)
+    # print dis.dis(fact)
+
+    fact2 = make_tail_recursive(fact2)
+    dis.dis(fact2)
+    print fact2(1000, 1)
+    # fact3 = make_tail_recursive(fact3)
