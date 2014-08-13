091ac631e20360652e123fe032b274eb8d18c220
Paul Tagliamonte <tag@pault.ag>
2014-08-13 00:00:01 -0400

    check that we have unoptimized (very hardcoded)

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,3 +1,6 @@
+import dis
+import io
+
 from .bytes import get_code
 
 
@@ -7,3 +10,13 @@ def test_code_load():
         pass
     x.__code__ = code
     assert x() == 3  # Lololol.
+
+
+def test_code_unoptimized():
+    code = get_code()
+    stream = io.StringIO()
+    dis.disco(code, file=stream)
+    stream.seek(0)
+    buf = stream.read()
+    assert "0 LOAD_CONST               1 (1)" in buf
+    assert "3 LOAD_CONST               2 (2)" in buf
